8aafb7a7db904663ce68ac2e5e055126f4b7c38c
Andrei Likhoded <liberty102and8131186@gmail.com>
2022-07-27 20:33:57 +0600

    in process

--- __MACOSX/Car.java
+++ __MACOSX/Car.java
@@ -1,9 +1,54 @@
 package kz.attractor.java.__MACOSX;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public class Car {
+    private String carName;
+    private int carNumber;
+
+    private State state = State.WAY;
 
 
-
+    public Car(String carName, int carNumber){
+        this.carName = carName;
+        this.carNumber = carNumber;
     }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(int carNumber) {
+        this.carNumber = carNumber;
+    }
+    public static List<Car> randomCarInParking() {
+        String car = "Audi, Porshe, Zhiguli, Toyota, Honda, Genesis";
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+
+            cars.add(new Car(car.substring(new Random().nextInt(car.length())), i + 1));
+        }
+        return cars;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carName='" + carName + '\'' +
+                ", carNumber=" + carNumber +
+                ", state=" + state +
+                '}';
+    }
+}
--- __MACOSX/Parking.java
+++ __MACOSX/Parking.java
@@ -1,4 +1,27 @@
 package kz.attractor.java.__MACOSX;
 
 public class Parking {
+    private int time;
+    private double money;
+
+    public Parking(int time, double money){
+        this.time = time;
+        this.money = money;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
 }
